8df3c280b531671d3fb4b772c6733ca94f47f3ee
zero-id <abdullatiffebriliana15@gmail.com>
2023-04-11 12:53:13 +0700

    Finish

--- src/handler.js
+++ src/handler.js
@@ -71,12 +71,38 @@ const addBook = (request, h) => {
   }
 };
 
-const getAllBooks = () => ({
-  status: "success",
-  data: {
-    books,
-  },
-});
+const getAllBooks = (request, h) => {
+  const { name, reading, finished } = request.query;
+
+  let filteredBooks = books;
+
+  if (name !== undefined) {
+    filteredBooks = filteredBooks.filter((book) => book
+      .name.toLowerCase().includes(name.toLowerCase()));
+  }
+
+  if (reading !== undefined) {
+    filteredBooks = filteredBooks.filter((book) => book.reading === !!Number(reading));
+  }
+
+  if (finished !== undefined) {
+    filteredBooks = filteredBooks.filter((book) => book.finished === !!Number(finished));
+  }
+
+  const response = h.response({
+    status: 'success',
+    data: {
+      books: filteredBooks.map((book) => ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+      })),
+    },
+  });
+  response.code(200);
+
+  return response;
+};
 
 const getBookById = (request, h) => {
   const { id } = request.params;
